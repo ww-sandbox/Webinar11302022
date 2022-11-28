7acfe0f8640b7d4aa071cec996e94ad3cde6f57e
Wojciech Wroblewski <wuwu@op.pl>
2022-11-28 21:20:13 +0100

    code after webinar

--- src/test/java/SwitchToTests.java
+++ src/test/java/SwitchToTests.java
@@ -23,20 +23,29 @@ public class SwitchToTests {
     @BeforeClass
     public void setUp(){
         driver = new FirefoxDriver();
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(2));
     }
 
     @Test
     public void switchToIframeTest(){
         driver.get("https://testautomationpractice.blogspot.com/");
 //        driver.findElement(By.cssSelector("input#RESULT_TextField-1"));
+//        przed przełączeniem element jest 'niewidoczny' dla drivera
+
+        WebElement formFrame = driver.findElement(By.cssSelector("iframe#frame-one1434677811"));
         driver.switchTo().frame("frame-one1434677811");
+//        driver.switchTo().frame(formFrame);
+//        dwa sposobny na przełączenie do iframe'a
         WebElement label = driver.findElement(By.xpath("//label[text()='First Name']"));
         driver.findElement(with(By.cssSelector("input"))
                 .below(label))
                 .sendKeys("Wojtek");
+//        lokalizacja elementu wewnątrz ramki z wykorzystaniem lokalizatorów relatywnych oraz wysłanie tekstu do
+//        wyszukanego elementu
 
         driver.switchTo().parentFrame();
+//        przełączenie do ramki bazowej
+        driver.findElement(By.xpath("//button[text()=\"Click Me\"]"));
     }
 
     @Test
@@ -46,7 +55,8 @@ public class SwitchToTests {
         driver.findElement(By.xpath("//button[contains(., 'JS Alert')]")).click();
 
         Alert alert = driver.switchTo().alert();
-        alert.accept();
+//        alert.accept();
+        alert.dismiss();
         WebElement message = driver.findElement(By.cssSelector("#result"));
         Assert.assertEquals(message.getText(),"You successfully clicked an alert");
     }
@@ -55,14 +65,18 @@ public class SwitchToTests {
     public void switchToTab(){
         driver.get("https://the-internet.herokuapp.com/windows");
         String originalWindow = driver.getWindowHandle();
+//        Pobranie uchwytu (identyfikatora) okna domyślnego/bazowego
 
         Assert.assertEquals(driver.getWindowHandles().size(), 1);
         Assert.assertEquals(driver.getTitle(), "The Internet");
+//        upewnienie sie, że otwarte jest tylko jedno okno z określonym tytułem
 
         driver.findElement(By.linkText("Click Here")).click();
+//        kliknięcie przycisku otwierającego nowe okno
 
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(3));
         wait.until(ExpectedConditions.numberOfWindowsToBe(2));
+//        oczekiwanie na otworzenie nowego okna
 
         for (String windowHandle : driver.getWindowHandles()) {
             if(!originalWindow.contentEquals(windowHandle)) {
@@ -70,11 +84,17 @@ public class SwitchToTests {
                 break;
             }
         }
+//        iteracja po kolejnych uchwytach (identyfikatorach) okien w celu odnalezienia uchwytu nowego okna oraz
+//        przełączenie do niego
 
         wait.until(ExpectedConditions.titleIs("New Window"));
+//        oczekiwanie na poprawne załadowanie tytułu nowego okna
 
         driver.close();
+//        zamknięcie aktualnego okna (otwartego w trakcie testu)
         driver.switchTo().window(originalWindow);
+//        przełączenie do pierwotnego okna - bez tego kroku pozostaniemy w zamkniętym oknie - driver będzie odpowiadał
+//        błędem
         Assert.assertEquals(driver.getTitle(), "The Internet");
 
     }
--- src/test/java/WaitTests.java
+++ src/test/java/WaitTests.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.*;
+import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
@@ -16,7 +17,9 @@ public class WaitTests {
     @BeforeClass
     public void setUp(){
         driver = new FirefoxDriver();
-//        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+//        Czas przez jaki driver będzie oczekiwał podczas wyszukiwania każdego elementu zanim zwróci błąd
+        driver.manage().window().maximize();
     }
 
     @Test
@@ -30,7 +33,13 @@ public class WaitTests {
         driver.get("https://demoqa.com/progress-bar");
         driver.findElement(By.cssSelector("button#startStopButton")).click();
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(15));
-        wait.until(ExpectedConditions.attributeToBe(By.cssSelector("div.progress-bar"), "aria-valuenow", "100"));
+//        Deklaracja waita wraz z maksymalnym czasem, który będzie on oczekiwał na spełnienie określonego warunku
+//        wait.until(ExpectedConditions.attributeToBe(By.cssSelector("div.progress-bar"), "aria-valuenow", "100"));
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector("#resetButton")));
+//        deklaracja warunku na jaki będzie oczekiwał driver zanim zostanie rzucony TimeoutException. Najczęściej
+//        wykorzystuje się, dedykowaną dla Selenium, bibliotekę ExpectedConditions
+//        W powyższych przykładach oczekiwanie na odpowiednią wartośc atrybutu webelementu oraz na pojawienie się w DOM
+//        webelementu spełniający dany lokalizator
     }
 
     @Test
@@ -42,6 +51,30 @@ public class WaitTests {
                 .pollingEvery(Duration.ofSeconds(1))
                 .ignoring(NoSuchElementException.class);
         WebElement resetButton = wait.until(driver -> {return driver.findElement(By.cssSelector("#resetButton"));});
+//        Fluent wait jest najbardziej elastycznym roadzajem waita, ale jest dość skomplikowany i mniej czytelny.
+//        Zdecydowaną większość przypadków można zrealizować z wykorzystaniem Explicit waita
+    }
+
+    @Test
+    public void dynamicLoadingTest(){
+        driver.get("https://the-internet.herokuapp.com/dynamic_loading/1");
+        driver.findElement(By.cssSelector("#start button")).click();
+//        wyszukiwanie i kliknięcie w przycis uruchamiający loader
+
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(2));
+//        deklaracja waita
+        WebElement loadingBar = driver.findElement(By.cssSelector("div#loading"));
+//        deklaracja elementu reprezentującego pasek ładowania
+        wait.until(ExpectedConditions.visibilityOf(loadingBar));
+//        oczekiwanie na widoczność paska ładowania (wcześniej obecny w DOM)
+        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+//        zmiana parametrów waita
+        wait.until(ExpectedConditions.invisibilityOf(loadingBar));
+//        oczekiwanie na zniknięcie paska ładowania
+
+        String finalMessage = driver.findElement(By.cssSelector("#finish h4")).getText();
+        Assert.assertEquals(finalMessage, "Hello World!");
+//        Sprawdzenie czy pojawia się odpowiedni tekst na koniec ładowania
     }
 
     @AfterClass
